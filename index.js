3e69810e884e4d41020d815b6780c0926ec78cdb
binhus <eng.robsonwilli@gmail.com>
2020-11-10 22:49:18 -0300

    primeiro desafio

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const readline = require('readline-sync');
 const altura = readline.questionFloat('Digite sua altura: ');
 const peso = readline.questionFloat('Digite seu peso: ');
 
-console.log(Math.pow(altura, 2));
 const imc = (peso / Math.pow(altura, 2)).toFixed(2);
 
 console.log(`Seu IMC é : ${imc}`);

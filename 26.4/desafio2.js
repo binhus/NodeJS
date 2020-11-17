ce2330632be844813e77814d75f670fbf7effae3
binhus <eng.robsonwilli@gmail.com>
2020-11-16 21:03:42 -0300

    corrigindo desafio 1 e implementando o 2

--- 26.4/desafio2.js
+++ 26.4/desafio2.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const app = express();
+app.get('/', (_req, res) => {
+  res.send({ message: 'pong' });
+});
 
 app.listen(3000, () => {
   console.log('O pai tá ON');
